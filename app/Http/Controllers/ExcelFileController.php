4a9650a3416028fe3af3a813faa7e43163f39df8
S15SoKo <s15soko@onet.pl>
2019-09-26 12:02:19 +0200

    Create/ Store functions with web routing

--- app/Http/Controllers/ExcelFileController.php
+++ app/Http/Controllers/ExcelFileController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Session;
 
 class ExcelFileController extends Controller
 {
+    /**
+     * @return array
+     */
+    public static function allowedExcelFileTypes()
+    {
+        $allowedFileType = ['application/vnd.ms-excel', 'text/xls', 'text/xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+        return $allowedFileType;
+    }
+
     /**
      * Display a file content (if exist)
      */
@@ -19,18 +30,42 @@ class ExcelFileController extends Controller
      */
     public function create()
     {
-        
+        return view('excel.create');
     }
 
-    public function store()
+    /**
+     * On success redirect to main page
+     */
+    public function store(Request $request)
     {
-        
+        $uploadedFile = $request->file("file");
+        if(!$uploadedFile){
+            Session::flash('message', "File not found"); 
+            return redirect()->back();
+        }
+
+        if(in_array($uploadedFile->getMimeType(), self::allowedExcelFileTypes()) !== true){
+            Session::flash('message', "Wrong file type"); 
+            return redirect()->back();
+        }
+
+        try {  
+            // store to default laravel storage place
+            $uploadedFile->storeAs("files/excel", "myExcelFile.xlsx");
+
+            return redirect("/");
+
+        } catch (\Throwable $th) {
+            Session::flash('message', "File saving has been failed"); 
+            return redirect()->back();
+        }
     }
+
 
     /**
      * Show form for editing the specified resource.
      */
-    public function edit()
+    public function edit(Request $request)
     {
         
     }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('excel.index');
 });
+
+Route::get("/create", "ExcelFileController@create");
+Route::post("/store", "ExcelFileController@store");
+
