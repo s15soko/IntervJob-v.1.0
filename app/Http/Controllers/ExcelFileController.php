886b89eaca716d27fc94cc578ca4373a5e01fe1d
S15SoKo <s15soko@onet.pl>
2019-09-26 11:44:23 +0200

    Excel file controller with default functions

---
+++ app/Http/Controllers/ExcelFileController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExcelFileController extends Controller
+{
+    /**
+     * Display a file content (if exist)
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show form for uploading a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    public function store()
+    {
+        
+    }
+
+    /**
+     * Show form for editing the specified resource.
+     */
+    public function edit()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+}
