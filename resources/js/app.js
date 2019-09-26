4c6c6810ededc9497ee31af3bd6c5984dc8dfbf2
S15SoKo <s15soko@onet.pl>
2019-09-26 16:16:19 +0200

    Setup js app files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
 require('./bootstrap');
+
+import { ExcelFileCalculationsController } from "./controllers/ExcelFileCalculationsController";
+
+
+$(function(){
+
+    var excelFileCalculationsController = new ExcelFileCalculationsController();
+    excelFileCalculationsController.init();
+});
+
+
+
